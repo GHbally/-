52dc0cdea79f489fa05d2263e975862bcecbf5d8
appkey <sitoyq1689@gmail.com>
2024-06-05 19:29:53 +0900

    텔레봇 추가

---
+++ 프로그램/noti.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import sys
+import time
+import sqlite3
+import telepot
+from pprint import pprint
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+from datetime import date, datetime, timedelta
+import traceback
+import lxml
+
+key = 'F240513145' #'여기에 API KEY를 입력하세요'
+TOKEN = '7375661036:AAHcts7hIskYwfHc3s65lc6uO3pFQjyLG9I' #'여기에 텔레그램 토큰을 입력하세요'
+MAX_MSG_LENGTH = 300
+baseurl = 'http://www.opinet.co.kr/api/lowTop10.do?out=xml&code='+key
+bot = telepot.Bot(TOKEN)
+
+#XXXXXX&prodcd=B027&area=0101&cnt=2
+
+def getData(prod_type, loc_param='1508'):
+    res_list = []
+    url = baseurl + '&prodcd=' + prod_type + '&area=' + loc_param + '&cnt=20'
+    res_body = urlopen(url).read()
+    soup = BeautifulSoup(res_body, 'lxml-xml')  # 여기서 lxml-xml 파서를 사용합니다.
+    items = soup.findAll('OIL')  # 'item' 대신 'oil' 태그로 변경
+    cnt = 0
+    for item in items:
+        price = item.find('PRICE').text
+        new_adr = item.find('NEW_ADR').text
+        os_nm = item.find('OS_NM').text
+        row = str(cnt+1)+ '위 ' + f'이름: {os_nm}, 가격: {price}, 주소: {new_adr}'
+        res_list.append(row)
+        cnt = cnt +1
+    return res_list
+def sendMessage(user, msg):
+    try:
+        bot.sendMessage(user, msg)
+    except:
+        traceback.print_exc(file=sys.stdout)
+
+def run(date_param, param='11710'):
+    conn = sqlite3.connect('logs.db')
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS logs( user TEXT, log TEXT, PRIMARY KEY(user, log) )')
+    conn.commit()
+
+    user_cursor = sqlite3.connect('users.db').cursor()
+    user_cursor.execute('CREATE TABLE IF NOT EXISTS users( user TEXT, location TEXT, PRIMARY KEY(user, location) )')
+    user_cursor.execute('SELECT * from users')
+
+    for data in user_cursor.fetchall():
+        user, param = data[0], data[1]
+        print(user, date_param, param)
+        res_list = getData( param, date_param )
+        msg = ''
+        for r in res_list:
+            try:
+                cursor.execute('INSERT INTO logs (user,log) VALUES ("%s", "%s")'%(user,r))
+            except sqlite3.IntegrityError:
+                # 이미 해당 데이터가 있다는 것을 의미합니다.
+                pass
+            else:
+                print( str(datetime.now()).split('.')[0], r )
+                if len(r+msg)+1>MAX_MSG_LENGTH:
+                    sendMessage( user, msg )
+                    msg = r+'\n'
+                else:
+                    msg += r+'\n'
+        if msg:
+            sendMessage( user, msg )
+    conn.commit()
+
+if __name__=='__main__':
+    today = date.today()
+    current_month = today.strftime('%Y%m')
+
+    print( '[',today,']received token :', TOKEN )
+
+    pprint( bot.getMe() )
+
+    run(current_month)
---
+++ 프로그램/teller.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import sys
+import time
+import sqlite3
+import telepot
+from pprint import pprint
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+from datetime import date, datetime, timedelta
+import traceback
+import noti
+import pandas as pd
+
+file_path = "area_codes.xlsx"
+
+# 엑셀 파일 읽기
+
+df = pd.read_excel(file_path, converters={'C': str})
+
+# B 열과 C 열을 사용하여 딕셔너리 생성
+area_code_dict = dict(zip(df['구'], df['코드']))
+
+# 딕셔너리 확인
+print(area_code_dict)
+
+def replyGasData(prod_type, user, loc_param='00'):
+    print(user, prod_type, loc_param)
+    res_list = noti.getData( prod_type, loc_param )
+    msg = ''
+    for r in res_list:
+        print( str(datetime.now()).split('.')[0], r )
+        if len(r+msg)+1>noti.MAX_MSG_LENGTH:
+            noti.sendMessage( user, msg )
+            msg = r+'\n'
+        else:
+            msg += r+'\n'
+    if msg:
+        noti.sendMessage( user, msg )
+    else:
+        noti.sendMessage( user, '%s 잘못된 질못 형식입니다.')
+
+def save( user, loc_param ):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS users( user TEXT, location TEXT, PRIMARY KEY(user, location) )')
+    try:
+        cursor.execute('INSERT INTO users(user, location) VALUES ("%s", "%s")' % (user, loc_param))
+    except sqlite3.IntegrityError:
+        noti.sendMessage( user, '이미 해당 정보가 저장되어 있습니다.' )
+        return
+    else:
+        noti.sendMessage( user, '저장되었습니다.' )
+        conn.commit()
+
+def check( user ):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS users( user TEXT, location TEXT, PRIMARY KEY(user, location) )')
+    cursor.execute('SELECT * from users WHERE user="%s"' % user)
+    for data in cursor.fetchall():
+        row = 'id:' + str(data[0]) + ', location:' + data[1]
+        noti.sendMessage( user, row )
+
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    if content_type != 'text':
+        noti.sendMessage(chat_id, '난 텍스트 이외의 메시지는 처리하지 못해요.')
+        return
+
+    text = msg['text']
+    args = text.split(' ')
+
+    if text.startswith('전국') and len(args) > 1:
+        print('try to 전국', args[1])
+        if args[1] == '휘발유':
+            replyGasData('B027',chat_id)
+        elif args[1] == '경유':
+            replyGasData('D047', chat_id)
+    elif text.startswith('지역') and len(args)>1:
+        print('try to 지역', args[1])
+        replyGasData( '201705', chat_id, args[1] )
+    elif text.startswith('저장')  and len(args)>1:
+        print('try to 저장', args[1])
+        save( chat_id, args[1] )
+    elif text.startswith('확인'):
+        print('try to 확인')
+        check( chat_id )
+    else:
+        noti.sendMessage(chat_id, '모르는 명령어입니다.\n지역 [기름 종류]을 입력해주세요')
+
+
+today = date.today()
+current_month = today.strftime('%Y%m')
+
+print( '[',today,']received token :', noti.TOKEN )
+
+bot = telepot.Bot(noti.TOKEN)
+pprint( bot.getMe() )
+
+bot.message_loop(handle)
+
+print('Listening...')
+
+while 1:
+  time.sleep(10)
